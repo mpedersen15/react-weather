d7aef3c9e549b5391edef534f19468839854ebe8
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-05-06 08:54:57 -0600

    wired up WeatherForm and Message to handle state change in Weather

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,9 +1,11 @@
 var React = require('react');
 
 var WeatherMessage = React.createClass({
+	
 	render: function(){
+		var {temp, location} = this.props;
 		return (
-			<h3>Weather message!</h3>
+			<h3>It's {temp} degrees F in {location}!</h3>
 		);
 	}
 });
