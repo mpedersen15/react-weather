12eefe02438e97da1863e6c74070d294f32c268b
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-05-07 08:00:53 -0600

    completed API call to OpenWeatherMap

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,24 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=2153618fec01a6aebf6f8f80d376978f&units=imperial';
+
+module.exports = {
+	getTemp: function(location){
+		var encodedLocation = encodeURIComponent(location);
+		var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+		return axios.get(requestUrl).then(
+			function(res){
+				console.log('success', res);
+				if (res.data.cod && res.data.message){
+					throw new Error(res.data.message);
+				}else{
+					return res.data.main.temp;
+				}
+			},
+			function(res){
+				console.log('error', res);
+				throw new Error(res.data.message);
+			}
+		);
+	}
+}
